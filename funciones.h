447489fe5763e24ca32fae0304cb4f4e6c956d1d
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-05-26 01:14:49 -0500

    Update funciones.h
    Se agregaron las funciones "pedir" , "cambio", "fin_del_juego" y "verificar

--- funciones.h
+++ funciones.h
@@ -12,5 +12,9 @@ typedef char tipo;
 void menu_del_juego(tipo arr[][9]);
 void tableros(tipo arr1[][9],int opcion);
 void imprimir(tipo arr2[][9]);
+void pedir(tipo arr5[][9]);
+void cambio(tipo arr3[][9],int fil1,int col1 ,int fil2 ,int col2);
+bool fin_del_juego(tipo arr4[][9]);
+void verificar(tipo arr6[][9], int f1 ,int c1, int f2 , int c2);
 
 #endif //UNTITLED_FUNCIONES_H
