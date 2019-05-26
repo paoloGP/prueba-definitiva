c5abd27c4f775e4c5471012d83e34185a7f12c5d
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-05-26 01:27:56 -0500

    Update main.cpp
    se agregan las funciones que se agregaron a funciones.h

--- main.cpp
+++ main.cpp
@@ -6,5 +6,10 @@ using namespace std;
 int main() {
     tipo tablero[9][9] = {};
     menu_del_juego(tablero);
+    do{
+        pedir(tablero);
+        cout << endl;
+    }while(!fin_del_juego(tablero));
+    cout << "Fin del juego, !Lo lograste¡";
     return 0;
 }
