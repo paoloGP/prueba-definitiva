5e33075e94d226dd2f72d6e150888935ee67cfd8
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-05-27 12:45:10 -0500

    Update funciones.h
    se cambió el parámetro por referencia "anti" por "sa".

--- funciones.h
+++ funciones.h
@@ -12,7 +12,7 @@ typedef char tipo;
 void menu_del_juego(tipo arr[][9]);
 void tableros(tipo arr1[][9],int opcion);
 void imprimir(tipo arr2[][9]);
-void pedir(tipo arr5[][9],int &anti);
+void pedir(tipo arr5[][9],int &sa);
 void cambio(tipo arr3[][9],int fil1,int col1 ,int fil2 ,int col2);
 bool fin_del_juego(tipo arr4[][9]);
 void verificar(tipo arr6[][9], int fi1 ,int co1, int fi2 , int co2);
