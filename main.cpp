865df1c4ca3f9c8eb4c155d172fcc795585aeb85
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-05-27 12:16:55 -0500

    Update main.cpp
    Se añadió la variable "antinfinito" y los mensajes correspondientes.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,28 @@ using namespace std;
 int main() {
     tipo tablero[9][9] = {};
     menu_del_juego(tablero);
-    do{
-        pedir(tablero);
-        cout << endl;
-    }while(!fin_del_juego(tablero));
-    int i =0;
-    for(int a = 0; a<9;a++){
-        for(int b = 0;b<9;b++){
-            if(tablero[a][b] == 'O')
-                i = i + 1;
+    if(tablero[0][0] == 'x') {
+        int antinfinito =0;
+        while(!fin_del_juego(tablero) && antinfinito < 1)
+            pedir(tablero , antinfinito);
+
+        if(antinfinito < 1) {
+            int i = 0;
+            for (int a = 0; a < 9; a++) {
+                for (int b = 0; b < 9; b++) {
+                    if (tablero[a][b] == 'O')
+                        i = i + 1;
+                }
+            }
+            if (i > 1)
+                cout << "FIN DEL JUEGO, te falto muy poco!!!";
+            else
+                cout << "Lo lograste !!!";
         }
+        else
+            cout << endl << "Adios";
     }
-    if(i > 1)
-        cout << "FIN DEL JUEGO, te falto muy poco!!!";
     else
-        cout << "Lo lograste !!!";
+        cout << endl << "Gracias por su tiempo.";
     return 0;
 }
